409f49e72fc670870e583164ba2ae3a1cdc2b308
wolfsilver <wolfsilver@users.noreply.github.com>
2024-08-31 03:11:01 +0000

    feat: remove AccessToken

--- memogram.go
+++ memogram.go
@@ -7,7 +7,6 @@ import (
 	"log/slog"
 	"net/http"
 	"path/filepath"
-	"strconv"
 	"strings"
 	"time"
 
@@ -65,21 +64,6 @@ func NewService() (*Service, error) {
 		return nil, errors.Wrap(err, "failed to create bot")
 	}
 	s.bot = b
-
-	if config.AccessToken != "" {
-		parts := strings.Split(config.AccessToken, ":")
-		if len(parts) == 2 {
-			userIDStr := parts[0]
-			accessToken := parts[1]
-			userID, err := strconv.ParseInt(userIDStr, 10, 64)
-			if err != nil {
-				slog.Error("failed to parse userID to int64", slog.String("userID", userIDStr))
-				return nil, err
-			}
-			userAccessTokenCache.Store(userID, accessToken)
-			slog.Info("load accessToken", slog.Any("userID", userID))
-		}
-	}
 
 	return s, nil
 }
